7e30e64dc65c70b344a72b15c5ee02b5550d659c
LoGiCa7 <47193468+LoGiCa7@users.noreply.github.com>
2026-07-01 00:13:49 +0100

    Update import path for views package

--- app/about.go
+++ app/about.go
@@ -4,7 +4,7 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/matheusgomes28/urchin/common"
 	"github.com/matheusgomes28/urchin/database"
-	"github.com/matheusgomes28/urchin/views"
+	"./views"
 )
 
 func aboutHandler(c *gin.Context, app_settings common.AppSettings, db database.Database) ([]byte, error) {
